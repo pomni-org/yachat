71c0e9f0eaa2ccb9a8a888c9aa93c6719dbdef4e
Помни <zgsu3589@gmail.com>
2026-07-15 11:30:53 +0700

    Wire presence assets into Vercel build

--- scripts/build-vercel.cjs
+++ scripts/build-vercel.cjs
@@ -23,6 +23,20 @@ async function copyFile(name) {
   await fs.copyFile(path.join(rendererDir, name), path.join(outputDir, name));
 }
 
+async function injectPresenceAssets() {
+  const indexPath = path.join(outputDir, "index.html");
+  const html = await fs.readFile(indexPath, "utf8");
+  const withStyles = html.replace(
+    '<link rel="stylesheet" href="./styles.css" />',
+    '<link rel="stylesheet" href="./styles.css" />\n    <link rel="stylesheet" href="./assets/chat-presence.css" />'
+  );
+  const withScript = withStyles.replace(
+    '<script src="./app.js"></script>',
+    '<script src="./app.js"></script>\n    <script src="./assets/chat-presence.js"></script>'
+  );
+  await fs.writeFile(indexPath, withScript, "utf8");
+}
+
 async function build() {
   await fs.rm(outputDir, { recursive: true, force: true });
   await fs.mkdir(outputDir, { recursive: true });
@@ -31,6 +45,7 @@ async function build() {
   await fs.cp(path.join(rendererDir, "assets"), path.join(outputDir, "assets"), {
     recursive: true
   });
+  await injectPresenceAssets();
 }
 
 build().catch((error) => {
