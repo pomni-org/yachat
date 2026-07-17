47f15cd1f1e79b840641af8a08bc66f52b05c146
Помни <zgsu3589@gmail.com>
2026-07-17 22:48:36 +0700

    fix(icons): add device sign-in key icon

--- src/renderer/assets/settings-icons.js
+++ src/renderer/assets/settings-icons.js
@@ -7,7 +7,7 @@
 
   Object.assign(ICONS, {
     "link-2": '<path d="M9 17H7a5 5 0 0 1 0-10h2" /><path d="M15 7h2a5 5 0 1 1 0 10h-2" /><path d="M8 12h8" />',
-    "message-square": '<path d="M21 15a4 4 0 0 1-4 4H8l-5 3V7a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4z" />',
+    "message-square": '<path d="M21 15a4 4 0 0 1-4 4H8l-5 3V7a4 4 0 0 1 4 4h10a4 4 0 0 1 4 4z" />',
     bookmark: '<path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />',
     folder: '<path d="M3 6a2 2 0 0 1 2-2h5l2 2h7a2 2 0 0 1 2 2v9a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3z" />',
     "battery-charging": '<path d="M15 7h1a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2h-2" /><path d="M22 11v2" /><path d="M6 7H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h2" /><path d="m11 6-3 6h4l-2 6 6-8h-4z" />',
@@ -15,6 +15,7 @@
     "wand-sparkles": '<path d="m15 4 5 5L8 21l-5-5z" /><path d="m14 5 5 5" /><path d="M6 4V2" /><path d="M6 10V8" /><path d="M3 6H1" /><path d="M11 6H9" /><path d="M19 19v-2" /><path d="M19 23v-2" /><path d="M17 21h-2" /><path d="M23 21h-2" />',
     "globe-2": ICONS.globe,
     "lock-keyhole": '<circle cx="12" cy="16" r="1" /><rect width="18" height="12" x="3" y="10" rx="2" /><path d="M7 10V7a5 5 0 0 1 10 0v3" />',
+    "key-round": '<path d="m15.5 7.5 2 2L21 6l-3-3-3.5 3.5" /><circle cx="9" cy="15" r="6" /><path d="m13.2 10.8 1.3-1.3" />',
     "circle-help": ICONS.help,
     info: '<circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" />',
     "bell-ring": '<path d="M10.268 21a2 2 0 0 0 3.464 0" /><path d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8a6 6 0 0 0-12 0c0 4.499-1.411 5.956-2.738 7.326" /><path d="M2 8c0-2.2.7-4.2 2-5.8" /><path d="M22 8c0-2.2-.7-4.2-2-5.8" />',
